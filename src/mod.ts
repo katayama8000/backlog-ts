d43d37830f0c8a17d72a1555b848ec6451270125
katayama8000 <tattu.0310@gmail.com>
2025-10-25 12:13:16 +0900

    feat: Update CI and release workflows to use actions/checkout@v5 and add linting checks

--- src/mod.ts
+++ src/mod.ts
@@ -3,44 +3,71 @@
  * @module
  */
 
-export * from "./config.ts";
-export * from "./entities.ts";
-export * from "./params.ts";
-export * from "./types.ts";
-export * from "./space.ts";
-export * from "./issue.ts";
+export * from './config.ts';
+export * from './entities.ts';
+export * from './params.ts';
+export * from './types.ts';
+export * from './space.ts';
+export * from './issue.ts';
 
-import type { BacklogConfig } from "./config.ts";
-import * as space from "./space.ts";
-import * as issue from "./issue.ts";
+import type { BacklogConfig } from './config.ts';
+import * as space from './space.ts';
+import * as issue from './issue.ts';
+
+/**
+ * Backlog client interface
+ */
+export interface BacklogClient {
+  // Space APIs
+  getSpace: () => ReturnType<typeof space.getSpace>;
+  getSpaceActivities: (
+    params: Parameters<typeof space.getSpaceActivities>[1]
+  ) => ReturnType<typeof space.getSpaceActivities>;
+  getSpaceIcon: () => ReturnType<typeof space.getSpaceIcon>;
+  getSpaceNotification: () => ReturnType<typeof space.getSpaceNotification>;
+  putSpaceNotification: (
+    params: Parameters<typeof space.putSpaceNotification>[1]
+  ) => ReturnType<typeof space.putSpaceNotification>;
+
+  // Issue APIs
+  postIssue: (
+    params: Parameters<typeof issue.postIssue>[1]
+  ) => ReturnType<typeof issue.postIssue>;
+  getIssue: (
+    issueIdOrKey: Parameters<typeof issue.getIssue>[1]
+  ) => ReturnType<typeof issue.getIssue>;
+  getIssues: (
+    params?: Parameters<typeof issue.getIssues>[1]
+  ) => ReturnType<typeof issue.getIssues>;
+  getIssueCount: (
+    params?: Parameters<typeof issue.getIssueCount>[1]
+  ) => ReturnType<typeof issue.getIssueCount>;
+}
 
 /**
  * Create Backlog API client
  */
-export function createClient(config: BacklogConfig) {
+export function createClient(config: BacklogConfig): BacklogClient {
   return {
     // Space APIs
     getSpace: () => space.getSpace(config),
     getSpaceActivities: (
-      params: Parameters<typeof space.getSpaceActivities>[1],
+      params: Parameters<typeof space.getSpaceActivities>[1]
     ) => space.getSpaceActivities(config, params),
     getSpaceIcon: () => space.getSpaceIcon(config),
     getSpaceNotification: () => space.getSpaceNotification(config),
     putSpaceNotification: (
-      params: Parameters<typeof space.putSpaceNotification>[1],
+      params: Parameters<typeof space.putSpaceNotification>[1]
     ) => space.putSpaceNotification(config, params),
 
     // Issue APIs
-    postIssue: (params: Parameters<typeof issue.postIssue>[1]) => issue.postIssue(config, params),
+    postIssue: (params: Parameters<typeof issue.postIssue>[1]) =>
+      issue.postIssue(config, params),
     getIssue: (issueIdOrKey: Parameters<typeof issue.getIssue>[1]) =>
       issue.getIssue(config, issueIdOrKey),
-    getIssues: (params?: Parameters<typeof issue.getIssues>[1]) => issue.getIssues(config, params),
+    getIssues: (params?: Parameters<typeof issue.getIssues>[1]) =>
+      issue.getIssues(config, params),
     getIssueCount: (params?: Parameters<typeof issue.getIssueCount>[1]) =>
       issue.getIssueCount(config, params),
   };
 }
-
-/**
- * Backlog client type
- */
-export type BacklogClient = ReturnType<typeof createClient>;
